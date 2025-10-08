39024837cfba29406068e952e631f25c56a5968e
Julien Flot <julienflot08@gmail.com>
2025-10-08 13:57:37 +0200

    feat: now only the filename is printed instead of the whole path in log traces

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -3,6 +3,8 @@
 
 #include <stdarg.h>
 
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
 typedef enum LoggerLevel {
     LogTrace = 0,
     LogInfo,
@@ -17,9 +19,9 @@ void logger_set_output(const char *path);
 void log_log(LoggerLevel lvl, const char *file, int line, const char *fmt, ...);
 void logger_stop();
 
-#define TRACE(...) log_log(LogTrace, __FILE__, __LINE__, __VA_ARGS__)
-#define INFO(...) log_log(LogInfo, __FILE__, __LINE__, __VA_ARGS__)
-#define WARN(...) log_log(LogWarn, __FILE__, __LINE__, __VA_ARGS__)
-#define ERROR(...) log_log(LogError, __FILE__, __LINE__, __VA_ARGS__)
+#define TRACE(...) log_log(LogTrace, __FILENAME__, __LINE__, __VA_ARGS__)
+#define INFO(...) log_log(LogInfo, __FILENAME__, __LINE__, __VA_ARGS__)
+#define WARN(...) log_log(LogWarn, __FILENAME__, __LINE__, __VA_ARGS__)
+#define ERROR(...) log_log(LogError, __FILENAME__, __LINE__, __VA_ARGS__)
 
 #endif /* LOG_H */
